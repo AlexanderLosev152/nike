9b719bc2e22da92987217906a0d1987e73ed3215
Alexander <losev.pavlovo@mail.ru>
2023-05-09 14:46:06 +0300

    9.05

--- js/main.js
+++ js/main.js
@@ -29,3 +29,26 @@ function prev() {
   index = (index + 1 + slides.length) % slides.length;
   slides[index].classList.add("active");
 }
+
+// cart
+
+document.querySelectorAll(".featured__img-1").forEach(image_1 =>{
+  image_1.addEventListener('click', () =>{
+    let src = image_1.getAttribute('src');
+    document.querySelector(".big_img-1").src = src;
+  })
+}); 
+
+document.querySelectorAll(".featured__img-2").forEach((image_2) => {
+  image_2.addEventListener("click", () => {
+    let src = image_2.getAttribute("src");
+    document.querySelector(".big_img-2").src = src;
+  });
+}); 
+
+document.querySelectorAll(".featured__img-3").forEach((image_3) => {
+  image_3.addEventListener("click", () => {
+    let src = image_3.getAttribute("src");
+    document.querySelector(".big_img-3").src = src;
+  });
+}); 
